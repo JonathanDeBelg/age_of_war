6a94a5b328c378cb0cd2cfc2ae6e62554e8d7465
Jonathan Vandionant <j.dionant@gmail.com>
2018-03-22 16:55:22 +0100

    tower start

---
+++ OOPG-ZetHierNaamVanJeSpel/src/main/java/nl/han/ica/ageofware/Tower.java
@@ -0,0 +1,33 @@
+package nl.han.ica.ageofware;
+
+
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.GameObject;
+import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
+import processing.core.PGraphics;
+
+public class Tower extends GameObject {
+
+    private int x, y, health, saldo;
+    
+
+    /**
+     * @param sprite The image which will be drawn whenever the draw method of the Tile is called.
+     */
+    public Tower(Sprite sprite) {
+        super(sprite);
+    }
+
+    void drawTower(){
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(PGraphics g) {
+
+    }
+}
