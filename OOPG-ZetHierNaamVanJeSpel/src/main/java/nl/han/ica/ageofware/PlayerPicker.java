dc29252feb9275507801d731ada4febb7e118066
Jonathan Vandionant <j.dionant@gmail.com>
2018-03-29 14:49:27 +0200

    timer voor playerspawner gemaakt

--- OOPG-ZetHierNaamVanJeSpel/src/main/java/nl/han/ica/ageofware/PlayerPicker.java
+++ OOPG-ZetHierNaamVanJeSpel/src/main/java/nl/han/ica/ageofware/PlayerPicker.java
@@ -8,6 +8,7 @@ public class PlayerPicker extends GameObject {
     private AgeOfWar aow;
     private int xPos;
     private int yPos;
+    private long prevMillis = 0;
 
     public PlayerPicker(AgeOfWar aow) {
         this.aow = aow;
@@ -24,18 +25,29 @@ public class PlayerPicker extends GameObject {
 
     @Override
     public void keyReleased(int keyCode, char key) {
-        generatePlayer(keyCode);
+        spawnPlayer(keyCode);
     }
 
-    private void generatePlayer(int keyCode) {
+    private void spawnPlayer(int keyCode) {
+        long currentMillis = System.currentTimeMillis();
+
+        if ((currentMillis - prevMillis) >= 2500){
+            generatePlayer(keyCode);
+            prevMillis = currentMillis;
+        } else {
+            System.out.println("De timer is nog niet voorbij");
+        }
+    }
+
+    private void generatePlayer(int keyCode){
         Character c = new Zombie(1);
-        if(keyCode == 49) {
+        if (keyCode == 49) {
             c = new Zombie(1);
-        } else if(keyCode == 50) {
+        } else if (keyCode == 50) {
             System.out.println("NINJA");
-        } else if(keyCode == 51) {
+        } else if (keyCode == 51) {
             System.out.println("Vogel");
-        } else if(keyCode == 52) {
+        } else if (keyCode == 52) {
             System.out.println("RIDDER");
         }
         aow.addGameObject(c, xPos, yPos);
